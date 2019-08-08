e65f40b987f20eeba7a55a05851d1225bf8605b2
Alan Teles de Oliveira <alantls@hotmail.com.br>
2019-08-08 19:36:06 -0400

    Inicio do PRojeto Validação

---
+++ Models/Compromisso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAspNetCoreMvcValidation.Models
+{
+    public class Compromisso
+    {
+        public string NomeCliente;
+        public DateTime Data;
+        public Boolean AceitaTermos;
+    }
+}
